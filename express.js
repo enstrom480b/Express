192425e51b06c96b3e786c2c7dc62ea71440b1d4
enstrom480b <64636784+enstrom480b@users.noreply.github.com>
2021-01-22 00:28:03 +0300

    Add files via upload

--- express.js
+++ express.js
@@ -2,6 +2,8 @@ var express =require('express')
 var path=require('path')
 var helmet=require('helmet')
 var router=express()
+const cookieParser=require('cookie-parser')
+router.use(cookieParser())
 var ejs=require('ejs')
 router.use(express.static('public'))
 router.use(express.static(__dirname + '/public'));
@@ -23,14 +25,39 @@ router.get('/about',function(req,res,next){
 
  })
  router.get('/login',function(req,res,next){
-
+     console.log(req.query)
     res.render('login')
 
  })
  router.post('/process_login',function(req,res,next){
-    res.json(req.body)
+const username=req.body.username
+const password=req.body.password
+if(password==='x')
+{
+   res.cookie('username',username)
+res.redirect('/welcome')
+}
+
+else{
+
+   res.redirect('/login?msg=fail&test=hello')
+ 
+
+}
 
  })
+
+ router.get('/welcome',function(req,res,next){
+   res.render('welcome',{
+   username:req.cookies.username
+})
+
+})
+
+router.get('/logout',function(req,res,next){
+   res.clearCookie('username')
+res.redirect('/login')
+})
 
 router.get('/ajax',function(req,res,next){
     res.render('ajax',{title:'Home'})
